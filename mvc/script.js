420b6a93d8b8ade1c9ddab4bd92d7512ebb15e48
a19enrriloli <a19enrriloli@inspedralbes.cat>
2021-12-10 12:40:34 +0100

    script.js

---
+++ mvc/script.js
@@ -0,0 +1,29 @@
+$('·registrar').click(function(){
+    var username = $('#usuarioreg').val();
+    var password = $('#passreg').val();
+    var confirmpassword = $('#confirmpassreg').val();
+    //AQUI
+    $.ajax({
+        url: "php/register.php",
+        type: "post",
+        data: {
+            username: username,
+            password: password,
+            confirmpassword: confirmpassword
+        },
+        success: function(result){
+            var msg= "";
+
+            if (result=='OK'){
+                printLogged();
+
+            }else if(result == "REGISTROINCORRECTO"){
+                msg= "El nombre usuario ya existe o las contraseñas no son iguales";   
+            }else{
+                msg="Los campos no pueden estar vacíos";
+            }
+            $("#messageReg").html(msg);
+        }
+    });
+
+});
