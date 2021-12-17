7bc5c0bcc6a21c0a45cc919d45e8bbc86ce50748
a20valzavvas <a20valzavvas@inspedralbes.cat>
2021-12-17 10:40:54 +0100

    bbdd email agregado

--- back/sql/create.sql
+++ back/sql/create.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Temps de generació: 09-12-2021 a les 09:17:17
+-- Temps de generació: 17-12-2021 a les 10:39:57
 -- Versió del servidor: 10.4.21-MariaDB
 -- Versió de PHP: 8.0.10
 
@@ -36,13 +36,6 @@ CREATE TABLE `peli` (
   `puntuacio` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Bolcament de dades per a la taula `peli`
---
-
-INSERT INTO `peli` (`Poster`, `Title`, `Type`, `Year`, `imdbID`, `puntuacio`) VALUES
-('https://m.media-amazon.com/images/M/MV5BMTg5NzY0Mz', 'Cars', 'movie', 2006, 'tt0317219', 20);
-
 -- --------------------------------------------------------
 
 --
@@ -53,13 +46,6 @@ CREATE TABLE `puntuacio` (
   `numpun` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Bolcament de dades per a la taula `puntuacio`
---
-
-INSERT INTO `puntuacio` (`numpun`) VALUES
-(20);
-
 -- --------------------------------------------------------
 
 --
@@ -67,18 +53,20 @@ INSERT INTO `puntuacio` (`numpun`) VALUES
 --
 
 CREATE TABLE `usuari` (
-  `id` int(10) NOT NULL,
-  `nom` varchar(20) NOT NULL,
-  `contrasena` varchar(20) NOT NULL,
-  `puntuacio` int(20) NOT NULL
+  `id` int(30) NOT NULL,
+  `nom` varchar(15) NOT NULL,
+  `contrasena` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Bolcament de dades per a la taula `usuari`
 --
 
-INSERT INTO `usuari` (`id`, `nom`, `contrasena`, `puntuacio`) VALUES
-(0, 'user', '123', 20);
+INSERT INTO `usuari` (`id`, `nom`, `contrasena`, `email`) VALUES
+(33, 'user1', '12345', 'a20mohwafche@inspedralbes.cat'),
+(34, 'valeria', '12356', 'a20mohwafche@inspedralbes.cat'),
+(35, 'cheryl', '12345', 'a20valzavvas@inspedralbes.cat');
 
 --
 -- Índexs per a les taules bolcades
@@ -101,9 +89,17 @@ ALTER TABLE `puntuacio`
 -- Índexs per a la taula `usuari`
 --
 ALTER TABLE `usuari`
-  ADD PRIMARY KEY (`puntuacio`,`id`),
-  ADD UNIQUE KEY `nom` (`nom`),
-  ADD UNIQUE KEY `contrasena` (`contrasena`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per les taules bolcades
+--
+
+--
+-- AUTO_INCREMENT per la taula `usuari`
+--
+ALTER TABLE `usuari`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- Restriccions per a les taules bolcades
@@ -114,12 +110,6 @@ ALTER TABLE `usuari`
 --
 ALTER TABLE `peli`
   ADD CONSTRAINT `peli_ibfk_1` FOREIGN KEY (`puntuacio`) REFERENCES `puntuacio` (`numpun`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Restriccions per a la taula `puntuacio`
---
-ALTER TABLE `puntuacio`
-  ADD CONSTRAINT `puntuacio_ibfk_1` FOREIGN KEY (`numpun`) REFERENCES `usuari` (`puntuacio`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
