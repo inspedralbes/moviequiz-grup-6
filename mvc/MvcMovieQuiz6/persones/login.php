9491d089424513dbd8de132a0d61b60a7fac513b
a19enrriloli <a19enrriloli@inspedralbes.cat>
2021-12-17 10:54:26 +0100

    login variables cambiadas

--- mvc/MvcMovieQuiz6/persones/login.php
+++ mvc/MvcMovieQuiz6/persones/login.php
@@ -4,19 +4,19 @@
 
   if(!empty($_POST['nom']) && !empty($_POST['contrasena'])) {
     $comp = array ("nom" => $_POST['nom'],"contrasena" => $_POST['contrasena']);
-    $user2 = new users();
+    $user2 = new usuari();
     $usuario = $user2->select($comp["nom"]);
 
     if($_POST['contrasena'] == $usuario[0]['contrasena']) {
       session_start();
-      $arr = array('exito' => true, 'nom' => $usuario[0]['nom'], "apellido" => $usuario[0]['apellido'], "correo" => $usuario[0]['correo']);
+      $arr = array('exito' => true, 'nom' => $usuario[0]['nom']);
       $_SESSION['usuario'] = $arr;
       $myJSON = json_encode($arr);
       echo $myJSON;
     }
     else {
       session_start();
-      $arr = array('exito' => false, 'correo' => $usuario[0]['correo'], "contrasena" => $usuario[0]['contrasena']);
+      $arr = array('exito' => false, "contrasena" => $usuario[0]['contrasena']);
       $myJSON = json_encode($arr);
       echo $myJSON;
     }
